0967cceb5cc56ebea98bcccd1909bb08eb1a44b7
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-31 15:36:56 +0200

    post

--- app.js
+++ app.js
@@ -11,3 +11,32 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Server del mio blog')
 })
+
+const post = [
+  {
+    titolo: "Italia",
+    contenuto: "Lorem ipsum dolor",
+    img: "/assets/italia.jpg",
+    tags: ["italia", "lorem"],
+  }, {
+    titolo: "Spagna",
+    contenuto: "Lorem ipsum dolor",
+    img: "/assets/spagna.jpg",
+    tags: ["spagna", "lorem"],
+  }, {
+    titolo: "Grecia",
+    contenuto: "Lorem ipsum dolor",
+    img: "/assets/grecia.jpg",
+    tags: ["grecia", "lorem"],
+  }, {
+    titolo: "Cuba",
+    contenuto: "Lorem ipsum dolor",
+    img: "/assets/cuba.jpg",
+    tags: ["cuba", "lorem"],
+  }, {
+    titolo: "Thailandia",
+    contenuto: "Lorem ipsum dolor",
+    img: "/assets/thailandia.jpg",
+    tags: ["thailandia", "lorem"],
+  }
+]
